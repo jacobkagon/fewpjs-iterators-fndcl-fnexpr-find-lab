efe58bf7e0792742126518bdd44194d3fef40efc
JakesProjects1 <jacob@kagon.net>
2020-11-16 19:17:30 -0500

    done

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array){
+  const win = array.find( function(record){
+    return record["result"] == "W"
+  });
+
+  return win && win["year"]
+}
+
